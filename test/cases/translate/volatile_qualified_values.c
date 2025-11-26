f6e5556f2dc9ec83863db4993d8caba08b3b1487
Theo Fabi <fabi.theo@gmail.com>
2025-11-26 17:47:23 -0500

    wip

--- test/cases/translate/volatile_qualified_values.c
+++ test/cases/translate/volatile_qualified_values.c
@@ -2,6 +2,7 @@ typedef volatile unsigned int vuint;
 
 static volatile unsigned int v1 = 0u;
 static vuint v2 = 0u;
+volatile unsigned int *vptr = &v1;
 
 struct hw {
     int x;
@@ -20,6 +21,7 @@ void foo() {
     unsigned int x = v1;
     x = v2;
     x += v2;
+    x = *vptr;
     v2 = 3u;
     v2 += 1u;
     unsigned int z = v2 += 1u;
@@ -37,34 +39,45 @@ void baz() {
 // translate
 //
 // pub const vuint = __helpers.Volatile(c_uint);
+//
 // pub var v1: __helpers.Volatile(c_uint) = .{
 //     .value = 0,
 // };
+//
 // pub var v2: vuint = .{
 //     .value = 0,
 // };
+//
+// pub export var vptr: [*c]volatile c_uint = @as([*c]volatile c_uint, @ptrCast(&v1));
+//
 // pub const struct_hw = extern struct {
 //     x: c_int = 0,
 // };
+//
 // pub const union_uhw = extern union {
 //     x: c_int,
 // };
+//
 // pub var hw: __helpers.Volatile(struct_hw) = .{
 //     .value = struct_hw{
 //         .x = 0,
 //     },
 // };
+//
 // pub var uhw: __helpers.Volatile(union_uhw) = .{
 //     .value = union_uhw{
 //         .x = 0,
 //     },
 // };
+//
 // pub extern fn bar(a: c_uint, b: c_uint) void;
+//
 // pub export fn foo() void {
 //     var x: c_uint = __helpers.addVolatile(&v1).*;
 //     _ = &x;
 //     x = __helpers.addVolatile(&v2).*;
 //     x +%= __helpers.addVolatile(&v2).*;
+//     x = vptr.*;
 //     __helpers.addVolatile(&v2).* = 3;
 //     __helpers.addVolatile(&v2).* +%= 1;
 //     var z: c_uint = blk: {
@@ -78,11 +91,13 @@ void baz() {
 //     var yy: c_int = __helpers.addVolatile(&hw).*.x;
 //     _ = &yy;
 // }
+//
 // pub var varr: [3]vuint = [3]vuint{
 //     0,
 //     0,
 //     0,
 // };
+//
 // pub export fn baz() void {
 //     var x: c_uint = __helpers.addVolatile(&varr[@as(c_int, 2)]).*;
 //     _ = &x;
